941d446caa447023038b89d85e61bef3ff2477a1
Kumar <sumitkumar52@deloitte.com>
2022-09-12 18:27:37 +0530

    update 24

--- function.sh
+++ function.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 function handler () {
-    echo "Echoing from shell script"
-    anypoint-cli cloudhub load-balancer list
-    aws configure import --csv "file://~/aws_credentials.csv"
-    aws s3 ls --no-verify-ssl
+    # echo "Echoing from shell script"
+    # anypoint-cli cloudhub load-balancer list
+    # aws configure import --csv "file://~/aws_credentials.csv"
+    # aws s3 ls --no-verify-ssl
+    ACCESS_KEY=$1
+    SECRET_KEY=$2
+    echo "$ACCESS_KEY"
+    echo "$SECRET_KEY"
 }
