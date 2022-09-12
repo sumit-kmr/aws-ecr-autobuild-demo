46a865cdaa9605d47ea8309fda68182873ce816c
Kumar <sumitkumar52@deloitte.com>
2022-09-12 17:13:20 +0530

    update 20

--- function.sh
+++ function.sh
@@ -3,6 +3,5 @@
 function handler () {
     echo "Echoing from shell script"
     anypoint-cli cloudhub load-balancer list
-    aws configure import --csv file://~/aws_credentials.csv
     aws s3 ls --no-verify-ssl
 }
