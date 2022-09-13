e20a811e395ae47981afa307878f905f2c8588f5
Kumar <sumitkumar52@deloitte.com>
2022-09-13 13:34:19 +0530

    update 38

--- function.sh
+++ function.sh
@@ -3,12 +3,10 @@
 function handler () {
     # echo "Echoing from shell script"
     # anypoint-cli cloudhub load-balancer list
-    # aws configure import --csv "file://~/aws_credentials.csv"
-    ls ~/
-    ls ~/.aws
     export AWS_ACCESS_KEY_ID=$1
     export AWS_SECRET_ACCESS_KEY=$2
     export AWS_DEFAULT_REGION=$3
     #aws s3 --region $3 ls --no-verify-ssl
     aws configure list
+    find / -type d -name ".aws"
 }
