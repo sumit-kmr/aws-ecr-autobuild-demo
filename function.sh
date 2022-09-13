86283096d6f98110c9755d05cefd0e264e0e41a1
Kumar <sumitkumar52@deloitte.com>
2022-09-13 16:21:48 +0530

    update 44

--- function.sh
+++ function.sh
@@ -3,9 +3,9 @@
 function handler () {
     # echo "Echoing from shell script"
     # anypoint-cli cloudhub load-balancer list
-    # export AWS_ACCESS_KEY_ID=$1
-    # export AWS_SECRET_ACCESS_KEY=$2
-    # export AWS_DEFAULT_REGION=$3
+    export AWS_ACCESS_KEY_ID=$1
+    export AWS_SECRET_ACCESS_KEY=$2
+    export AWS_DEFAULT_REGION=$3
     echo '[default]\naws_access_key_id=$1\naws_secret_access_key=$2' > /tmp/aws_credentials
     echo '[default]\nregion=$3\noutput=json' > /tmp/aws_config
     export AWS_SHARED_CREDENTIALS_FILE=/tmp/aws_credentials
