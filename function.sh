1c607f0af78a08e01fd01adb54e02c097ae85130
Kumar <sumitkumar52@deloitte.com>
2022-09-13 17:58:42 +0530

    update 47

--- function.sh
+++ function.sh
@@ -6,10 +6,11 @@ function handler () {
     export AWS_ACCESS_KEY_ID=$1
     export AWS_SECRET_ACCESS_KEY=$2
     export AWS_DEFAULT_REGION=$3
-    echo '[default]\naws_access_key_id=$1\naws_secret_access_key=$2' > /tmp/aws_credentials
-    echo '[default]\nregion=$3\noutput=json' > /tmp/aws_config
+    echo '[user1]\naws_access_key_id=$1\naws_secret_access_key=$2' > /tmp/aws_credentials
+    echo '[profile user1]\nregion=$3\noutput=json' > /tmp/aws_config
     export AWS_SHARED_CREDENTIALS_FILE=/tmp/aws_credentials
     export AWS_CONFIG_FILE=/tmp/aws_config
+    export AWS_PROFILE=user1
     aws configure list
     #aws s3 ls --no-verify-ssl
     #aws s3api get-object --bucket anypoint-dlb-cert-bucket --key test.txt /tmp/froms3.txt
