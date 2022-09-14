b89a1aa5d9f6e48cdfdc6261e67c95c5a2b7456d
Kumar <sumitkumar52@deloitte.com>
2022-09-14 21:21:36 +0530

    update 69

--- function.sh
+++ function.sh
@@ -13,7 +13,7 @@ echo "aws access key: $AWS_ACCESS_KEY"
 echo "aws secret key: $AWS_SECRET_KEY"
 
 # Download file from S3
-./s3-get.sh  "anypoint-dlb-cert-bucket" "$AWS_REGION" "test.txt" "/tmp"
+~/s3-get.sh  "anypoint-dlb-cert-bucket" "$AWS_REGION" "test.txt" "/tmp"
 
 echo "Downloaded file content: "
 cat /tmp/test.txt
