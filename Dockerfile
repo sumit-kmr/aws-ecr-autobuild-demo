14ec14c3ae7727b55ffdb3c05dbb546773fad831
Kumar <sumitkumar52@deloitte.com>
2022-09-16 23:58:47 +0530

    update 79

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,17 @@
-# FROM public.ecr.aws/lambda/nodejs:12
-# FROM node:17-alpine3.14
-# FROM public.ecr.aws/docker/library/ubuntu:18.04
 FROM ubuntu
 RUN apt-get update
 RUN apt-get install -y ntpdate
 RUN apt-get install -y libssl-dev
 RUN apt-get install -y curl
 RUN apt-get install -y npm
-# RUN apk update
-# RUN apk add --update curl
-# RUN apk add --update openssl
 
 # Download and configure anypoint-cli
-# RUN npm install -g yarn
-# RUN npm install -g anypoint-cli@latest
-# COPY credentials "~/.anypoint/credentials"
-# COPY index.js "/var/lang/lib/node_modules/anypoint-cli/node_modules/home-dir/index.js"
+RUN npm install -g yarn
+RUN npm install -g anypoint-cli@latest
+COPY credentials "~/.anypoint/credentials"
+COPY index.js "/usr/local/lib/node_modules/anypoint-cli/node_modules/home-dir/index.js"
 
+# Get args from build and set env variables
 ARG AWS_ACCESS_KEY_ID
 ARG AWS_SECRET_ACCESS_KEY
 ARG AWS_REGION
@@ -24,19 +19,7 @@ ENV AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID
 ENV AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY
 ENV AWS_REGION=$AWS_REGION
 
-# Copy our bootstrap and make it executable
-# WORKDIR /var/runtime/
-# COPY bootstrap bootstrap
-# RUN chmod 755 bootstrap
-
-# Copy our function code and make it executable
-# WORKDIR /var/task/
-# COPY function.sh function.sh
-# RUN chmod 755 function.sh
-# RUN npm install crypto-js
-# CMD [ "function.sh.handler" ]
-
+# Copy the script and make it executable
 COPY lambda_fun.sh "~/"
-COPY credentials "~/.anypoint/credentials"
 RUN ["chmod", "+x", "~/lambda_fun.sh"]
 ENTRYPOINT ["~/lambda_fun.sh"]
--- lambda_fun.sh
+++ lambda_fun.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ~/
+ls
 cd /tmp
 
 HEADERS="$(mktemp)"
@@ -12,6 +14,8 @@ CURRENT_DATE_ISO8601="${CURRENT_DATE_DAY}T$(date -u '+%H%M%S')Z"
 EMPTY_STRING_HASH="$(printf "" | openssl dgst -sha256 | sed 's/^.* //')"
 AWS_SERVICE='s3'
 AWS_S3_BUCKET_NAME='anypoint-dlb-cert-bucket'
+ANYPOINT_DLB_NAME='wdapi-sbx-dlb-us-west'
+declare -a s3_keys # declare array
 
 # Create an SHA-256 hash in hexadecimal.
 # Usage:
@@ -74,11 +78,99 @@ ${EMPTY_STRING_HASH}"
         -f -S -o ${OUT_FILE}
 }
 
-download_s3_file "test.txt" "fromS3.txt"
+# List files from s3 folder.
+# Usage:
+#   list_s3_files <folder_name>
+function list_s3_files {
+  AWS_SERVICE_ENDPOINT_URL="${AWS_S3_BUCKET_NAME}.${AWS_SERVICE}.${AWS_REGION}.amazonaws.com"
+	HTTP_CANONICAL_REQUEST_URI="/"
+	HTTP_CANONICAL_QUERY_STRING="list-type=2&prefix=${1}"
+	HTTP_CANONICAL_REQUEST_HEADERS="host:${AWS_SERVICE_ENDPOINT_URL}
+x-amz-content-sha256:${EMPTY_STRING_HASH}
+x-amz-date:${CURRENT_DATE_ISO8601}"
+	HTTP_REQUEST_SIGNED_HEADERS="host;x-amz-content-sha256;x-amz-date"
+	HTTP_CANONICAL_REQUEST="GET
+${HTTP_CANONICAL_REQUEST_URI}
+${HTTP_CANONICAL_QUERY_STRING}
+${HTTP_CANONICAL_REQUEST_HEADERS}\n
+${HTTP_REQUEST_SIGNED_HEADERS}
+${EMPTY_STRING_HASH}"
+	SIGNATURE="$(create_signature "${HTTP_CANONICAL_REQUEST}" | tail -c 65)"
+  HTTP_REQUEST_AUTHORIZATION_HEADER="\
+AWS4-HMAC-SHA256 Credential=${AWS_ACCESS_KEY}/${CURRENT_DATE_DAY}/\
+${AWS_REGION}/${AWS_SERVICE}/aws4_request, \
+SignedHeaders=${HTTP_REQUEST_SIGNED_HEADERS}, Signature=${SIGNATURE}"
 
-echo "Downloaded file content: "
-cat fromS3.txt
-ls
+  temp_xml_file="lists3resp.xml"
+  curl "https://${AWS_SERVICE_ENDPOINT_URL}${HTTP_CANONICAL_REQUEST_URI}?${HTTP_CANONICAL_QUERY_STRING}" \
+    -H "Authorization: ${HTTP_REQUEST_AUTHORIZATION_HEADER}" \
+    -H "x-amz-content-sha256: ${EMPTY_STRING_HASH}" \
+    -H "x-amz-date: ${CURRENT_DATE_ISO8601}" \
+    -k -sS -o ${temp_xml_file}
+	
+	
+  read_xml () { local IFS=\> ; read -d \< TAG VALUE ;}
+
+  s3_keys=()
+  i=0
+  while read_xml; do
+    if [[ $TAG == "Key" && $VALUE != "${1}/" ]]; then
+      s3_keys[i++]=$VALUE
+      fi
+  done < $temp_xml_file
+  rm $temp_xml_file
+}
+
+
+mkdir temp
+
+# Download all ca-certs in a temp folder
+echo "\nDownloading client certificates...\n"
+list_s3_files "ca-certs"
+for s3_key in "${s3_keys[@]}"
+do
+	file_name=${s3_key##*/}
+	download_s3_file "${s3_key}" "temp/${file_name}"
+  echo "Downloaded: ${file_name}"
+done
+
+# Merge ca certs
+echo "\nMerging the certificates...\n"
+merged_certs_name="bundle.pem"
+cat temp/* > ${merged_certs_name}
+rm temp/*
+mv ${merged_certs_name} temp/${merged_certs_name}
+
+# Download dlb cert and private key
+echo "\nDownloading DLB certificate and private key...\n"
+cert_file_name="public.pem"
+private_key_file_name="private.pem"
+list_s3_files "dlb-cert"
+for s3_key in "${s3_keys[@]}"
+do
+	file_name=${s3_key##*/}
+	if [[ $file_name == *"public"* ]]; then
+		download_s3_file "${s3_key}" "temp/$cert_file_name"
+	else
+		download_s3_file "${s3_key}" "temp/$private_key_file_name"
+	fi
+  echo "Downloaded: ${file_name}"
+done
+
+# Get common name of the certificate
+cert_name=$(openssl x509 -noout -subject -in temp/$cert_file_name)
+cert_name=$(echo "${cert_name##*=}" | xargs)
+
+# Replace the certificate
+echo "\nDeleting cert ${cert_name}..."
+anypoint-cli cloudhub load-balancer ssl-endpoint remove ${ANYPOINT_DLB_NAME} ${cert_name}
+echo "Uploading updated cert ${cert_name}..."
+anypoint-cli cloudhub load-balancer ssl-endpoint add --clientCertificate "temp/${merged_certs_name}" --verificationMode on $ANYPOINT_DLB_NAME "temp/${cert_file_name}" "temp/${private_key_file_name}" 
+echo "Setting ${cert_name} as default certificate..."
+anypoint-cli cloudhub load-balancer ssl-endpoint set-default $ANYPOINT_DLB_NAME $cert_name
+echo "\nCertificate replaced successfully!"
+
+rm -r temp
 
 # Send the response
 curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response" -d "response from fun"
