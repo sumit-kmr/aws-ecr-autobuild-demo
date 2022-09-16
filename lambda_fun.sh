f8f4e4471f509fdae07fb95686beeb526ed2ba52
Kumar <sumitkumar52@deloitte.com>
2022-09-17 00:17:10 +0530

    update 81

--- lambda_fun.sh
+++ lambda_fun.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cd ~/
-ls
-cd /tmp
-ls
 
 HEADERS="$(mktemp)"
 # Get the lambda invocation event
 EVENT_DATA=$(curl -sS -LD "$HEADERS" -X GET "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next")
 # Extract request ID by scraping response headers received above
 REQUEST_ID=$(grep -Fi Lambda-Runtime-Aws-Request-Id "$HEADERS" | tr -d '[:space:]' | cut -d: -f2)
+cd ~/
+ls
+cd /tmp
+ls
 # Declare constants
 CURRENT_DATE_DAY="$(date -u '+%Y%m%d')"
 CURRENT_DATE_ISO8601="${CURRENT_DATE_DAY}T$(date -u '+%H%M%S')Z"
